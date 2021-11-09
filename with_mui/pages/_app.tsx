44f9306490b320002196d5f60e7baba860c11f49
Per Enström <enstrom.per@gmail.com>
2021-11-09 19:44:40 +0100

    Update server rendering

--- with_mui/pages/_app.tsx
+++ with_mui/pages/_app.tsx
@@ -1,39 +1,34 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
-import {
-  StyledEngineProvider,
-  ThemeProvider,
-  CssBaseline
-} from '@mui/material';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
 import { theme } from 'styles/theme';
 
-function MyApp({ Component, pageProps }) {
+const clientSideEmotionCache = createCache({ key: 'css', prepend: true });
+
+function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps
+}) {
   const router = useRouter();
 
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
   return (
-    <>
+    <CacheProvider value={emotionCache}>
       <Head>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
         />
       </Head>
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Component {...pageProps} key={router.asPath} />
-        </ThemeProvider>
-      </StyledEngineProvider>
-    </>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} key={router.asPath} />
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
